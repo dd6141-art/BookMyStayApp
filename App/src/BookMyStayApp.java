085e0531f35e62e9bf596cb680868ec7b0593075
dd6141-art <dd6141@srmist.edu.in>
2026-03-05 15:24:22 +0530

    Dhanush Added UC-2

--- App/src/BookMyStayApp.java
+++ App/src/BookMyStayApp.java
@@ -1,5 +1,58 @@
+abstract class Room {
+    protected int numberOfBeds;
+    protected int squareFeet;
+    protected double pricePerNight;
+
+    public Room(int numberOfBeds, int squareFeet, double pricePerNight) {
+        this.numberOfBeds = numberOfBeds;
+        this.squareFeet = squareFeet;
+        this.pricePerNight = pricePerNight;
+    }
+
+    public void displayRoomDetails(int availability) {
+        System.out.println(getClass().getSimpleName() + " Details:");
+        System.out.println("Beds: " + numberOfBeds);
+        System.out.println("Size: " + squareFeet + " sq.ft");
+        System.out.println("Price per Night: ₹" + pricePerNight);
+        System.out.println("Available Rooms: " + availability);
+        System.out.println("-----------------------------");
+    }
+}
+
+class SingleRoom extends Room {
+    public SingleRoom(int beds, int size, double price) {
+        super(beds, size, price);
+    }
+}
+
+class DoubleRoom extends Room {
+    public DoubleRoom(int beds, int size, double price) {
+        super(beds, size, price);
+    }
+}
+
+class SuiteRoom extends Room {
+    public SuiteRoom(int beds, int size, double price) {
+        super(beds, size, price);
+    }
+}
+
 public class BookMyStayApp {
-    public static void main(String[] args){
-        System.out.println("Hotel Booking System v1.0");
+    public static void main(String[] args) {
+        System.out.println("Hotel Booking System v2.1.000");
+
+        int singleRoomAvailability = 5;
+        int doubleRoomAvailability = 3;
+        int suiteRoomAvailability = 2;
+
+        Room single = new SingleRoom(1, 200, 1000.0);
+        Room doubleR = new DoubleRoom(2, 350, 1800.0);
+        Room suite = new SuiteRoom(3, 600, 3500.0);
+
+        single.displayRoomDetails(singleRoomAvailability);
+        doubleR.displayRoomDetails(doubleRoomAvailability);
+        suite.displayRoomDetails(suiteRoomAvailability);
+
+        System.out.println("Application terminated.");
     }
 }
--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # BookMyStayApp
 UC-1 Added Welcome Message  
+UC-2 Added Basic Room Types and Availability  
