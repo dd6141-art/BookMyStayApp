dd989602e8fcfc0a805dbfe98c8a48fc03eb5613
dd6141-art <dd6141@srmist.edu.in>
2026-03-27 16:01:56 +0530

    Dhanush Added UC-6

--- App/src/BookMyStayApp.java
+++ App/src/BookMyStayApp.java
@@ -1,26 +1,26 @@
-
-import java.util.HashSet;
-import java.util.LinkedList;
+import java.util.*;
 
 public class BookMyStayApp {
-    public static void main(String[] var0) {
-        LinkedList var1 = new LinkedList();
-        var1.add("Customer_Alwyn");
-        var1.add("Customer_John");
-        HashSet var2 = new HashSet();
+    public static void main(String[] args) {
+
+        Queue<String> requests = new LinkedList<>();
+        requests.add("Customer_Alwyn");
+        requests.add("Customer_John");
+
+        Set<String> allocatedRooms = new HashSet<>();
+
         System.out.println("--- Processing Room Allocations ---");
 
-        while(!var1.isEmpty()) {
-            String var3 = (String)var1.poll();
-            int var10000 = 100 + var2.size();
-            String var4 = "ROOM-" + (var10000 + 1);
-            if (var2.add(var4)) {
-                System.out.println("SUCCESS: " + var3 + " assigned to " + var4);
+        while (!requests.isEmpty()) {
+            String customer = requests.poll();
+            String roomId = "ROOM-" + (100 + allocatedRooms.size() + 1);
+            if (allocatedRooms.add(roomId)) {
+                System.out.println("SUCCESS: " + customer + " assigned to " + roomId);
             } else {
-                System.out.println("ERROR: Room " + var4 + " is already occupied!");
+                System.out.println("ERROR: Room " + roomId + " is already occupied!");
             }
         }
 
-        System.out.println("\nTotal Rooms Allocated Today: " + var2.size());
+        System.out.println("\nTotal Rooms Allocated Today: " + allocatedRooms.size());
     }
 }
--- README.md
+++ README.md
@@ -4,3 +4,4 @@ UC-2 Added Basic Room Types and Availability
 UC-3 Added Centralized Room Inventory Management  
 UC-4 Added Room Search & Availability Check  
 UC-5 Added Booking Request (First-Come-First-Served)
+uc-6 Reservation Confirmation & Room Allocation  
